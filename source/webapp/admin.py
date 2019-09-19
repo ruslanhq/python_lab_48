49bb82c23454c24d3795b7a5a4e5dc709ac7dc97
Ruslan <h.ruslan97@gmail.com>
2019-09-19 20:35:55 +0600

    Added models Product, sign up model in admin-panel, fixture, and mirhations

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from webapp.models import Product
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'description', 'category', 'count', 'price']
+    list_filter = ['category', 'price']
+    list_display_links = ['pk', 'name']
+    search_fields = ['name', 'category']
+    fields = ['name', 'description', 'category', 'count', 'price']
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+
